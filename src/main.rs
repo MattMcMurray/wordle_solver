03f5a8f4b0f78a7bbee3980bb758aa30370a9b13
Matt McMurray <mathieumcmurray@gmail.com>
2022-01-29 14:29:28 -0600

    Read dict from txt file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::{
+    env,
+    fs::File,
+    io::{prelude::*, BufReader},
+    process,
+    path::Path
+};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    let lines = read_lines_from_file(Path::new(&config.wordfile));
+
+    println!("Read {} words from {}", lines.len(), config.wordfile)
+}
+
+struct Config {
+    wordfile: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+
+        let wordfile = args[1].clone();
+
+        Ok(Config { wordfile })
+    }
+}
+
+fn read_lines_from_file(filename: &Path) -> Vec<String> {
+    let file = File::open(&filename)
+        .unwrap_or_else(|_| panic!("No such file"));
+
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
 }
